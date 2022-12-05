d02bc9f96e00b302486f77d73208c35e9e1ffa3d
laurenwdk <109314270+laurenwdk@users.noreply.github.com>
2022-12-05 15:49:01 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const PORT = process.env.PORT || 8000;
+const app = express();
+const path = require("path");
+
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
+
+
